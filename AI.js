ef6c72bd45c21669d6092416e70aada90702b10a
KevinKerman1 <kevin.kerman01@gmail.com>
2025-01-14 08:11:23 -0700

    model modified to gpt 4o mini

--- AI.js
+++ AI.js
@@ -40,7 +40,7 @@ export async function sendToOpenAI(base64Images, res) {
         }));
 
         const response = await client.chat.completions.create({
-            model: "gpt-4o",
+            model: "gpt-4o-mini",
             temperature: 0.1,
             messages: [
                 {
